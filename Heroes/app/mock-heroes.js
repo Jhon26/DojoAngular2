b5124599687a747446838bfea74df59aa851a2de
Jhon26 <yonbabolivar@hotmail.com>
2016-04-02 12:39:55 -0500

    Dojo 2

---
+++ Heroes/app/mock-heroes.js
@@ -0,0 +1,25 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Heroes;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("Heroes", Heroes = [
+                { "id": "11", "name": "Batman" },
+                { "id:12": , "name": "Ironman" },
+                { "id:13": , "name": "Deadpool" },
+                { "id:14": , "name": "Profesor Super-O" },
+                { "id:15": , "name": "Cebichica" },
+                { "id:16": , "name": "Spiderman" },
+                { "id:17": , "name": "Captain America" }
+            ]);
+        }
+    }
+});
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
+//# sourceMappingURL=mock-heroes.js.map
